fe4d5e69a27121a763946fb0d6f807cbb1674918
SimonSassian <nukisimon1@gmail.com>
2024-03-06 08:27:37 -0500

    pildi peale vajutada

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,44 @@
-﻿namespace PokerClickerV3;
-
-public partial class MainPage : ContentPage
+﻿namespace PokerClickerV3
 {
-	int count = 0;
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+        public MainPage()
+        {
+            InitializeComponent();
+        }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        private async void OnPokerImageTapped(object sender, EventArgs e)
+        {
+            count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+            // Pildi suuruse muutmine animatsiooniga
+            await ((VisualElement)sender).ScaleTo(0.8, 250); // Muudab suurust 80% -le originaalist
+            await ((VisualElement)sender).ScaleTo(1, 250); // Taastab originaalsuuruse
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+            // Asendage SemanticScreenReader.Announce oma eelistatud teabeedastusmeetodiga
+            Announce($"Clicked {count} times");
+        }
+
+        // Funktsioon teabe edastamiseks (võib olla osa teisest teenusest)
+        private void Announce(string message)
+        {
+            // Asendage see osa oma rakenduse konkreetse teabe edastamise meetodiga
+            Console.WriteLine(message);
+        }
+
+        private void OnPokerImageClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            // Jälgime klõpsamise arvu, kuid ei muuda pildi lähtepilti
+            if (count == 1)
+            {
+                ((ImageButton)sender).Source = ImageSource.FromFile("pokerclicked.png");
+            }
+
+            SemanticScreenReader.Announce($"Clicked {count} times");
+        }
+    }
 }
-
