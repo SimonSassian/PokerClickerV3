42da38166b7b6ea7ab40a918976f8e5a9535cbe2
SimonSassian <nukisimon1@gmail.com>
2024-03-18 11:35:46 +0200

    Revert "Startpage finished"
    This reverts commit 05bfd39e00ca15562f868d9d2294d2fba0ad60bc.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,18 +1,1 @@
-﻿using PokerClickerV3.Views;
-
-namespace PokerClickerV3
-{
-
-
-  public partial class MainPage : ContentPage
-  {
-    private void PlayClicked(object sender, EventArgs e)
-    {
-        // Handle the Settings button click event
-        // For example, navigate to the Settings page
-        //Navigation.PushAsync(new SettingsPage());
-        Application.Current.MainPage = new NavigationPage(new NavPage());
-    }
-
-  }
-}
+﻿
