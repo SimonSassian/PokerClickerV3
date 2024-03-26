c352d99fa0028d001d4735914198d393ec5a0346
SimonSassian <nukisimon1@gmail.com>
2024-03-26 16:20:24 +0200

    settings korras

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,9 +1,48 @@
-namespace PokerClickerV3.Views;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using System;
 
-public partial class SettingsPage : ContentPage
+namespace PokerClickerV3
 {
-	public SettingsPage()
-	{
-		InitializeComponent();
-	}
+    public partial class SettingsPage : ContentPage
+    {
+        // Constructor
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        // Muusika sisse või välja lülitamine
+        private void ToggleMusicSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            // Kood muusika sisse või välja lülitamiseks
+            bool musicOn = e.Value;
+            if (musicOn)
+            {
+                // Lülita muusika sisse
+                // Näide: MusicPlayer.Play();
+            }
+            else
+            {
+                // Lülita muusika välja
+                // Näide: MusicPlayer.Stop();
+            }
+        }
+
+        // Tume või hele reþiimi vahetamine
+        private void ToggleDarkModeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            bool darkModeOn = e.Value;
+            if (darkModeOn)
+            {
+                // Lülita tume reþiim sisse
+                App.Current.UserAppTheme = AppTheme.Dark;
+            }
+            else
+            {
+                // Lülita hele reþiim sisse
+                App.Current.UserAppTheme = AppTheme.Light;
+            }
+        }
+    }
 }
